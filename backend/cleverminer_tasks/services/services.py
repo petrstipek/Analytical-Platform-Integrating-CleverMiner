67d401c6aff29cb964820509446468e296b6e5e3
petrstipek <petr@stip.cz>
2025-11-25 12:13:16 +0100

    setup(backend): cleverminer basic setup, and structure preparation

---
+++ backend/cleverminer_tasks/services/services.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from cleverminer import cleverminer, clm_vars, clm_lcut
+
+
+def run_demo_analysis():
+    df = pd.read_csv(
+        "https://www.cleverminer.org/data/accidents.zip",
+        encoding="cp1250",
+        sep="\t",
+    )
+    df = df[["Driver_Age_Band", "Sex", "Speed_limit", "Severity"]]
+
+    imputer = SimpleImputer(strategy="most_frequent")
+    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+
+    clm = cleverminer(
+        df=df,
+        proc="4ftMiner",
+        quantifiers={"Base": 2000, "aad": 0.5},
+        ante=clm_vars(["Driver_Age_Band", "Sex", "Speed_limit"]),
+        succ={
+            "attributes": [clm_lcut("Severity")],
+            "minlen": 1,
+            "maxlen": 1,
+            "type": "con",
+        },
+    )
+    return clm
