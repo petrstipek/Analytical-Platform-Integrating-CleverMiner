e7c9b5e20d53bb8d5e67d14b97371d93bb5c5828
petrstipek <petr@stip.cz>
2025-12-24 01:15:09 +0100

    feature(backend): dj_rest_auth provided endpoints for log in, log out and others...

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -33,4 +33,6 @@ urlpatterns = [
     ),
     path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
     path("api/", include("accounts.urls")),
+    path("api/auth/", include("dj_rest_auth.urls")),
+    path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
