c0e0f988f4a35d35e0d6c66fe088a51e05689c48
petrstipek <petr@stip.cz>
2025-12-24 01:57:42 +0100

    setup(frontend): cookie based session handling

--- frontend/cleverminer-web-application/app/lib/api-client.ts
+++ frontend/cleverminer-web-application/app/lib/api-client.ts
@@ -1,8 +1,18 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+apiClient.interceptors.request.use((config) => {
+  const token = Cookies.get('csrftoken');
+  if (token) {
+    config.headers['X-CSRFToken'] = token;
+  }
+  return config;
 });
