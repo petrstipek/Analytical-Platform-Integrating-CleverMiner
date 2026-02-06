19ff95a252230f9a305738c7c798e7908c430ffd
petrstipek <petr@stip.cz>
2026-02-06 16:52:22 +0100

    feature(backend): dataset deletion

--- backend/cleverminer_tasks/apps.py
+++ backend/cleverminer_tasks/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CleverminerTasksConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cleverminer_tasks"
+
+    def ready(self):
+        import cleverminer_tasks.signals  # noqa: F401
