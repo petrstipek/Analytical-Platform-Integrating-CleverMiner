c3be492b69bc1889971404bf5510fe664e64086c
petrstipek <petr@stip.cz>
2026-02-24 00:13:11 +0100

    feature(frontend): tabs update and package

--- frontend/cleverminer-web-application/app/modules/datasets/components/molecules/index.ts
+++ frontend/cleverminer-web-application/app/modules/datasets/components/molecules/index.ts
@@ -1,3 +1,5 @@
 export { default as ColumnCard } from './ColumnCard';
 export { default as DatasetUploadCard } from './DatasetUploadCard';
 export { default as DatasetDetailHeader } from './DatasetDetailHeader';
+export { default as BarChartHorizontal } from './charts/BarChartHorizontal';
+export { default as NumericHistogramBarChart } from './charts/NumericHistogramBarChart';
--- frontend/cleverminer-web-application/app/modules/datasets/components/organisms/index.ts
+++ frontend/cleverminer-web-application/app/modules/datasets/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export { default as DatasetAnalysisView } from './DatasetAnalysisView';
 export { default as DatasetPreview } from './DatasetPreview';
 export { default as DatasetColumnsAnalysisView } from './DatasetColumnsAnalysis';
+export { default as DatasetProfile } from './DatasetProfile';
