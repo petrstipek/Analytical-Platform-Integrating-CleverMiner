45d8c523cca8079f213453e6e47b143b1ae60a88
petrstipek <petr@stip.cz>
2026-03-24 23:30:45 +0100

    feature(frontend): navigation header updated to pill style

--- frontend/cleverminer-web-application/app/shared/components/ui/organisms/site-header.tsx
+++ frontend/cleverminer-web-application/app/shared/components/ui/organisms/site-header.tsx
@@ -17,42 +17,42 @@ export function PlatformSiteHeader() {
   const pathNames = location.pathname.split('/').filter((x) => x);
 
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-      <SidebarTrigger className="-ml-1" />
-      <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem className="hidden md:block">
-            <BreadcrumbLink asChild>
+    <div className="flex h-16 shrink-0 items-center px-4">
+      <div className="border-border/40 bg-background/80 flex items-center gap-4 rounded-full border px-3 py-3 shadow-sm backdrop-blur-sm">
+        <SidebarTrigger className="-ml-1 size-7" />
+        <Separator orientation="vertical" className="h-4 data-[orientation=vertical]:h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
               <BreadcrumbLink asChild>
                 <Link to="/home">Home</Link>
               </BreadcrumbLink>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+            </BreadcrumbItem>
 
-          {pathNames
-            .filter((value) => value !== 'home')
-            .map((value, index, filtered) => {
-              const last = index === filtered.length - 1;
-              const to = `/${pathNames.slice(0, pathNames.indexOf(value) + 1).join('/')}`;
-              const label = value.charAt(0).toUpperCase() + value.slice(1).replace(/-/g, ' ');
-              return (
-                <React.Fragment key={to}>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    {last ? (
-                      <BreadcrumbPage>{label}</BreadcrumbPage>
-                    ) : (
-                      <BreadcrumbLink asChild className="hidden md:block">
-                        <Link to={to}>{label}</Link>
-                      </BreadcrumbLink>
-                    )}
-                  </BreadcrumbItem>
-                </React.Fragment>
-              );
-            })}
-        </BreadcrumbList>
-      </Breadcrumb>
-    </header>
+            {pathNames
+              .filter((value) => value !== 'home')
+              .map((value, index, filtered) => {
+                const last = index === filtered.length - 1;
+                const to = `/${pathNames.slice(0, pathNames.indexOf(value) + 1).join('/')}`;
+                const label = value.charAt(0).toUpperCase() + value.slice(1).replace(/-/g, ' ');
+                return (
+                  <React.Fragment key={to}>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      {last ? (
+                        <BreadcrumbPage>{label}</BreadcrumbPage>
+                      ) : (
+                        <BreadcrumbLink asChild className="hidden md:block">
+                          <Link to={to}>{label}</Link>
+                        </BreadcrumbLink>
+                      )}
+                    </BreadcrumbItem>
+                  </React.Fragment>
+                );
+              })}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    </div>
   );
 }
